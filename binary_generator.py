6729028e6f58de2f5c6807ceeba9751781307747
Haipei <haipei.private@gmail.com>
2019-12-12 16:10:20 -0500

    2019.12.12

--- binary_generator.py
+++ binary_generator.py
@@ -1,7 +1,8 @@
 import numpy as np
-from numpy.random import *
-
-from FTI import NTI,confusion_matrix
+# from numpy.random import *
+from random import uniform,gauss
+from majority_voting import majority_voting
+from truth_inference import FTI
 
 def clipped(x,a,b):
 	if x<a:
@@ -10,123 +11,126 @@ def clipped(x,a,b):
 		return b
 	return x
 
-def data_generator(object_n,worker_n,category_n,compare_ratio=1.0,skewness_right=0.00):
+def disparity(truth,ground_truth,binary=True,threshold=0.5):
 
-	object_score=uniform(0,1,object_n)
+	m=len(ground_truth)
 
-	ranking=np.flip(np.array(object_score).argsort())
+	disparity=np.array([0.0 for i in range(0,m)])
 
-	objects=[[] for i in range(0,category_n)]
+	disparity_self=[[0.0,0.0] for i in range(0,m)]
 
-	portion=poisson(0.9,worker_n)*0.01
-
-	task_list=[]
-	for i in range(0,object_n):
-		for j in range(0,i):
-			if i==j or uniform(0,1)>compare_ratio:
+	for j in range(0,m):
+		for k in range(0,len(truth[j])):
+			if truth[j][k]==None:
 				continue
-			task_list.append((i,j))
+			disparity_self[j][1]+=1
+			if truth[j][k]>threshold:
+				disparity_self[j][0]+=1
+	for j in range(0,m):
+		disparity_self[j]=disparity_self[j][0]/disparity_self[j][1]
 
-	shuffle(task_list)
-	r=poisson(int(len(task_list)/category_n),category_n).tolist()
-	p=0
-	l=len(task_list)
-	for i in range(0,len(r)):
-		for j in range(0,r[i]):
-			objects[i].append(task_list[p])
-			p+=1
-			if p==l:
-				break
-		if p==l:
-			break
+	disparity_other=[[0.0,0.0] for i in range(0,m)]
+	for j_ in range(0,m):
+		for j in range(0,m):
+			if j==j_:
+				continue
+			for k in range(0,len(truth[j])):
+				if truth[j][k]==None:
+					continue
+				disparity_other[j_][1]+=1
+				if truth[j][k]>threshold:
+					disparity_other[j_][0]+=1
+	for j in range(0,m):
+		disparity_other[j]=disparity_other[j][0]/disparity_other[j][1]
 
-	bias=[]
-	for i in range(0,worker_n):
-		bias.append([])
-		for j in range(0,category_n):
-			bias[i].append(0.0)
-			# if uniform(0,1)<0.5:
-				# bias[i].append(poisson(10)*0.01)
-			# else:
-				# bias[i].append(-poisson(10)*0.01)
-	sigma=[]
-	for i in range(0,worker_n):
-		sigma.append(poisson(100)*0.01)
+	disparity=[]
+	for j in range(0,m):
+		disparity.append(disparity_self[j]-disparity_other[j])
+	disparity=np.array(disparity)
+
+	return disparity
+
+def data_generator(task_n,worker_n,category_n,ratio=0.2):
+
+	category_centers=[uniform(0.3,0.7) for i in range(category_n)]
 
 	truth=[]
-	for i in range(0,len(objects)):
+	for j in range(category_n):
 		truth.append([])
-		for j in range(0,len(objects[i])):
-			a=objects[i][j][0]
-			b=objects[i][j][1]
-			truth[-1].append(int(round(1.0*object_score[b]/(object_score[a]+object_score[b]))))
+		for k in range(task_n):
+			if uniform(0,1)>=category_centers[j]:
+				truth[-1].append(0)
+			else:
+				truth[-1].append(1)
 
-	count=[]
-	for i in range(0,len(objects)):
-		count.append([])
-		for j in range(0,len(objects[i])):
-			count[i].append(0)
+	sigma=[]
+	for i in range(worker_n):
+		sigma.append(uniform(0.01,3.0))
+
+	bias=[]
+	for i in range(worker_n):
+		bias.append([])
+		for j in range(0,category_n):
+			bias[-1].append(uniform(-0.5,0.5))
+
+	ans_each_cat=int(round(task_n*ratio))
 
 	answer=[]
-	for i in range(0,worker_n):
+	for i in range(worker_n):
 		answer.append([])
-		for j in range(0,category_n):
-			answer[i].append([])
-			for k in range(0,len(truth[j])):
-				answer[i][j].append(None)
+		for j in range(category_n):
+			answer[-1].append([])
+			for k in range(task_n):
+				answer[-1][-1].append(None)
+			for z in range(ans_each_cat):
+				k=(i*ans_each_cat+z)%task_n
+				ans=int(round(clipped(gauss(mu=truth[j][k]+bias[i][j],sigma=sigma[i]),0,1)))
+				answer[-1][-1][k]=ans
+	
+	return answer,truth
 
-	for j in range(0,category_n):
-		for k in range(0,len(truth[j])):
-				l=list(range(0,worker_n))
-				shuffle(l)
-				for i in l:
-					if uniform(0,1)<portion[i] or count[j][k]==0:
-						answer[i][j][k]=int(round(clipped(truth[j][k]+bias[i][j]+normal(0,sigma[i]),0.0,1.0)))
 
-	return answer,truth,objects,ranking,bias,sigma
-
-def MajorityVoting(answer):
-	votes=[]
-	for i in range(0,len(answer[0])):
-		votes.append([])
-		for j in range(0,len(answer[0][i])):
-			votes[-1].append([0,0])
-
-	for i in range(0,len(answer)):
-		for j in range(0,len(answer[i])):
-			for k in range(0,len(answer[i][j])):
-				if answer[i][j][k]!=None:
-					votes[j][k][answer[i][j][k]]+=1
-
-	for j in range(0,len(answer[0])):
-		for k in range(0,len(answer[0][j])):
-			votes[j][k]=np.argmax(votes[j][k])
-
-	return votes
 
 if __name__=='__main__':
 
-	turn=100
+	# task_n=10000
+	# worker_n=100
+	# category_n=2
 
-	res=np.array([0.0,0.0])
+	# while True:
 
-	for t in range(0,turn):
+	# 	answer,truth=data_generator(
+	# 		task_n=task_n,
+	# 		worker_n=worker_n,
+	# 		category_n=category_n,
+	# 		ratio=0.10
+	# 	)		
 
-		print(t)
+	# 	truth=truth
+	# 	mv_truth=majority_voting(answer)
 
-		answer,truth,objects,ranking,bias,sigma=data_generator(object_n=50,worker_n=30,category_n=10)	
+	# 	disp=disparity(truth=mv_truth,ground_truth=truth)
 
-		esti_truth,_,_,esti_quality=NTI(answer,VLDB=False)
+	# 	acc=(np.array(truth)==np.array(mv_truth)).sum()/(category_n*task_n)
 
-		TP,TN,FP,FN=confusion_matrix(inferred=esti_truth,truth=truth)
-		ti_acc=(TP+TN)/(TP+TN+FP+FN)
-		print('TruthInference Accuracy :',ti_acc)
+	# 	print(disp,acc)
 
-		esti_voting=MajorityVoting(answer)
-		TP,TN,FP,FN=confusion_matrix(inferred=esti_voting,truth=truth)
-		mv_acc=(TP+TN)/(TP+TN+FP+FN)
-		print('MajorityVoting Accuracy :',mv_acc)
+	# 	if abs(disp[0])>0.15 and acc<0.85:
+	# 		break
 
-		res+=np.array([ti_acc,mv_acc])
+	# f=open('synthetic_worker%d_task%d_category%d.txt'%(worker_n,task_n,category_n),'w')
+	# f.write(str(answer)+'\n')
+	# f.write(str(truth))
+	# f.close()
 
-	print(res/turn)
+	f=open('synthetic_worker100_task10000_category2.txt')
+	answer=eval(f.readline())
+	truth=eval(f.readline())
+	f.close()
+
+	esti_truth,bias,sigma,quality=FTI(answer)
+
+	for j in range(len(esti_truth)):
+		for k in range(len(esti_truth[j])):
+			if esti_truth[j][k]==None:
+				print(j,k)
